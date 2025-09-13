b612c11f3cd82a86bfdf05684b947ba7b555ba24
Krishna teja 0904 <140784454+teja0604@users.noreply.github.com>
2025-09-13 12:55:57 +0530

    Updated files

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
 
       </div>
       <p className="py-4 text-center text-xs md:text-sm text-white/60">
-        Copyright 2024 © GreatStack. All Right Reserved.
+        Copyright 2025 © EduLearn Pro. All Right Reserved.
       </p>
     </footer>
   );
